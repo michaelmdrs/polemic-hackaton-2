878e42b241e36ba6cb29b588468efdfbad544775
michaelmdrs <mackelldeveloper@gmail.com>
2020-08-22 13:01:43 -0300

    Desafio2 Converter Centavos em Reais e Vice versa

---
+++ desafio2.py
@@ -0,0 +1,22 @@
+"""
+Desafio 2 - Conversor de Real para centavos - Vice versa
+"""
+print('-=' * 10)
+print('CONVESOR DE MOEDAS')
+print('-=' * 10)
+
+vlr_centavos = int(input('Digite o valor em centavos R$: '))
+vlr_reais = float(input('Digite o valor em reais R$: '))
+
+centavos = 10 * 10
+real = 10 * 10
+conversor_moedas = vlr_centavos / centavos
+conversor_reais = vlr_reais * real
+
+print(f'O valor de {vlr_centavos} centavos, convertido em reais é R$ {conversor_moedas}')
+print(f'O valor de R$ {vlr_reais:.1f} convertido, em centavos é {conversor_reais:.0f} centavos.')
+
+
+
+
+
